99114fd8eb7b029f305a4b6454a119d7138ac29f
kelly102107 <71689279+kelly102107@users.noreply.github.com>
2020-11-03 15:23:34 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-for (let x = 0; x <= 4; x++) {
-    led.plot(x + 4, x)
-    basic.pause(200)
-    led.plot(x + 3, x)
+for (let i = 0; i <= 4; i++) {
+    for (let j = 0; j <= i + 1; j++) {
+        led.plot(4 - i + j, j)
+        basic.pause(100)
+    }
+}
+for (let i = 0; i <= 4; i++) {
+    for (let j = 0; j <= 4 - i; j++) {
+        led.plot(j, i + j + 1)
+        basic.pause(100)
+    }
 }
 basic.forever(function () {
 	
